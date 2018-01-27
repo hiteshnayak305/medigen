f336f885684fb3fde10efd82447550bae50013d1
Arpit Choudhary <arpitkumar147@gmail.com>
2018-01-27 07:40:27 +0530

    update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require_once("./config.php");
+//require_once("./config.php");
 require_once("./includes/header.php");
  ?>
 
-    <h1>Hello <small>world</small></h1>
+
 
 <?php
 require_once("./includes/footer.php");
